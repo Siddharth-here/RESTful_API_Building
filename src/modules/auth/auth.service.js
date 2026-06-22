366e2e1a80818ddcf1e9b239f9391fca03b68274
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-22 23:50:19 +0530

    webtoken insatllation

--- src/modules/auth/auth.service.js
+++ src/modules/auth/auth.service.js
@@ -1,8 +1,10 @@
+import ApiError from "../../common/utils/api-error.js"
+import User from "./auth.model.js"
+
 const register = async (name, email, password, role) => {
-    //do user registration
-    let userObj;
-    return userObj
-    
+
+    const existing = await User.findOne({email})
+    if (existing) throw ApiError.conflict("Email already exists") 
 }
 
 export {register}
