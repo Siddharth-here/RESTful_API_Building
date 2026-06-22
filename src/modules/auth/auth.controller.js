8511362ef2f6638efcf7543028f6d455579e6020
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-22 23:19:59 +0530

    updated

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,6 +1,10 @@
+import *as authService from "./auth.service.js"
+import ApiResponse from "../../common/utils/api-response.js"
 
 const register = async () => {
-    
+
+    const user = await authService.register(req.body)
+    ApiResponse.created(res, "Registration Success", user)
 }
 
 
