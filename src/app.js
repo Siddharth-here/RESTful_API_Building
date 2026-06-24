87c54fc4af347dd8c21b1657b4ee0e7893ec1247
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-24 21:55:49 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import express from "express"
 
-const app = express()
+const app = express() //initializes your web application so you can start defining how it handles HTTP requests (routes, middleware, etc.).
 
 export default app
