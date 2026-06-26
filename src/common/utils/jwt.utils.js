d59066cd9418aa1e424ce4461955d5f7a5630c6c
Siddharth-here <siddharthmohapatra2004@gmail.com>
2026-06-26 23:14:38 +0530

    jwt token created

--- src/common/utils/jwt.utils.js
+++ src/common/utils/jwt.utils.js
@@ -1,4 +1,21 @@
 import crypto from "crypto";
+import jwt from "jsonwebtoken";
+
+const generateAccessToken = (payload) => {
+  jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
+  });
+};
+
+const verifyAccessToken = (token) => {
+  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+};
+
+const generateRefreshToken = (payload) => {
+  jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "7d",
+  });
+};
 
 const generateResetToken = () => {
   const rawToken = crypto.randomBytes(32).toString("hex");
